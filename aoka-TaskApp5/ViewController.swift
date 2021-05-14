6bb613a7ca2ae040e725702c1e05c63e466b45f4
aoka <aoka.555.ibiza@gmail.com>
2021-05-15 00:06:30 +0900

    enum修正

--- aoka-TaskApp5/ViewController.swift
+++ aoka-TaskApp5/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
     
     @IBOutlet private weak var answerLabel: UILabel!
     
+    enum UserError: Error {
+        case emptyLeftTextField
+        case emptyRightTextField
+        case divisionByZero
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,35 +28,47 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculationButton(_ sender: Any) {
-        let number = Double(textField.text!) ?? 0
-        let number2 = Double(textField2.text!) ?? 0
-        
-        
-        
-        guard  !textField.text!.isEmpty else{
-            aleat(aleatText: "割られる数を入力して下さい")
-            return
+        do {
+            let answer = try calField()
+            answerLabel.text = String(answer)
+        } catch let error as UserError {
+            
+            switch error {
+            case .emptyLeftTextField:
+                aleat(aleatText: "割られる数を入力して下さい")
+            case .emptyRightTextField:
+                aleat(aleatText: "割る数を入力して下さい")
+            case .divisionByZero:
+                aleat(aleatText: "割る数には0を入力して下さい。")
+            
+            }
+        } catch {
+            print("その他エラー")
         }
-        
-        guard !textField2.text!.isEmpty else{
-            aleat(aleatText: "割る数を入力して下さい")
-            return
-        }
-        
-        guard number2 != 0 else {
-            aleat(aleatText: "割る数には0を入力しないで下さい")
-            return
-        }
-        
-        let answerText = number / number2
-        answerLabel.text = String(format: "%.5f", answerText)
     }
     
     private func aleat(aleatText: String) {
         let aleatAction = UIAlertController(title: "課題5", message: aleatText, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
+        
         aleatAction.addAction(action)
         present(aleatAction, animated: true, completion: nil)
+    }
+    
+    private func calField() throws -> Float {
+        guard  !textField.text!.isEmpty else{
+            throw UserError.emptyLeftTextField
+        }
+        guard !textField2.text!.isEmpty else{
+            throw UserError.emptyRightTextField
+        }
+        let number = Float(textField.text!) ?? 0
+        let number2 = Float(textField2.text!) ?? 0
+        
+        guard number2 != 0 else {
+            throw UserError.divisionByZero
+        }
+        return number / number2
     }
 }
